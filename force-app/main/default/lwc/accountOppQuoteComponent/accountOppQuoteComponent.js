1e576165597f08d35a679f63adcc22ab64bbceb7
lalitjanwa <34535072+lalitjanwa@users.noreply.github.com>
2024-10-27 18:17:42 +0530

    Update accountOppQuoteComponent.js

--- force-app/main/default/lwc/accountOppQuoteComponent/accountOppQuoteComponent.js
+++ force-app/main/default/lwc/accountOppQuoteComponent/accountOppQuoteComponent.js
@@ -1,11 +1,13 @@
 import { LightningElement, wire, api } from 'lwc';
 import fetchOpportunitiesWithQuotesAndItems from '@salesforce/apex/AccountOpportunityDataController.getOpportunitiesWithQuotesAndItems';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class AccountOppQuoteComponent extends LightningElement {
     @api recordId;
     opportunitiesWithQuotesAndItems;
     firstOpp;
 
+    //columns for the Quote Line Items
     columns = [
         { label: 'Product Name', fieldName: 'productName' },
         { label: 'Service Date', fieldName: 'ServiceDate', type: 'date' },
@@ -14,12 +16,13 @@ export default class AccountOppQuoteComponent extends LightningElement {
         { label: 'Total Price', fieldName: 'TotalPrice', type: 'currency' },
     ];
     
+    // wire property to fetch Opportunity, Quote and Quote Line Items
     @wire(fetchOpportunitiesWithQuotesAndItems, { accountId: '$recordId' })
     wiredOpportunities({ error, data }) {
         if (data) {
-            try{
             this.opportunitiesWithQuotesAndItems = JSON.parse(JSON.stringify(data));
-
+                
+            //loop to set first Quote for each Opportunity and Product Name inside Quote Line Items.
             this.opportunitiesWithQuotesAndItems.forEach(opp => {
                 opp.firstQuoteName = null;
                 if(opp.Quotes){
@@ -41,13 +44,14 @@ export default class AccountOppQuoteComponent extends LightningElement {
                     this.firstOpp = opp.Id; // Assign the first quote name
                 }
             });
-            console.log('this.opportunitiesWithQuotesAndItems ');
-            console.log(this.opportunitiesWithQuotesAndItems);
-            } catch(ex){
-                console.log(ex);
-            }
+        } else {
+            // Error toast to show error when get any issue in fatching data.
+            const event = new ShowToastEvent({
+                title: 'Error',
+                message: 'Error in loading Opportunity Data, Contact your Administrator for more details !',
+                variant : 'error'
+            });
+            this.dispatchEvent(event);
         }
     }
-
-
 }
